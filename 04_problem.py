ff4e1749899a18a0e8045993ce21cdb420d43cfd
Sahay-Snehil <strange.snehil22@icloud.com>
2025-06-12 11:01:28 +0530

    Updated

--- 04_problem.py
+++ 04_problem.py
@@ -1,8 +1,21 @@
-def factorial(n):
-    if n == 0 or n == 1:
-        return 1
-    else:
-        return n * factorial(n-1)
+# def factorial(n):
+#     if n == 0 or n == 1:
+#         return 1
+#     else:
+#         return n * factorial(n-1)
     
-n= int(input("Enter any number: "))
-print(factorial(n))
+# n= int(input("Enter any number: "))
+# print(factorial(n))
+
+"""Without Functions"""
+ 
+n = int(input("Enter any number: "))
+product = 1
+
+if n == 0:
+    print("Factorial of 0 is 1")
+else:
+    for i in range(1,n+1):
+        product *= i
+        i       += 1
+    print(f"Factorial of {n} is {product}")
