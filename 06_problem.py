9a1c9345e6280187dbb8d9b2fe16adac436632a3
Sahay-Snehil <strange.snehil22@icloud.com>
2025-06-17 00:29:28 +0530

    Updated

--- 06_problem.py
+++ 06_problem.py
@@ -1,6 +1,13 @@
+from functools import reduce
+
+# l = [5, 10, 25, 3, 8]
+# largest = l[0]
+# for i in l:
+#     if i > largest:
+#         largest = i
+# print(f"largest number in the list is {largest}")
+
+"""Using reduce"""
 l = [5, 10, 25, 3, 8]
-largest = l[0]
-for i in l:
-    if i > largest:
-        largest = i
-print(f"largest number in the list is {largest}")
+largest = reduce(lambda a,b: a if a>b else b,l)
+print(largest)
